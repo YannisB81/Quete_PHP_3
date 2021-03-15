a0e0a556624e233ecf50670c83c37e00d7b0ad7e
YannisB81 <74103907+YannisB81@users.noreply.github.com>
2021-03-15 12:28:30 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,11 +7,22 @@
       $message1 = "0@sn9sirppa@#?ia'jgtvryko1";
       $message2 = "q8e?wsellecif@#?sel@#?setuotpazdsy0*b9+mw@x1vj";
       $message3 = "aopi?sgnirts@#?sedhtg+p9l!";
-      $message = $message1;
-      $keyNumber = strlen($message)/2;
-      $subChain = substr($message, 5, $keyNumber);
-      $subChain = str_replace("@#?", ' ', $subChain);
-      echo strrev($subChain);
+      
+      $keyNumber1 = strlen($message1)/2;
+      $subChain1 = substr($message1, 5, $keyNumber1);
+      $subChain1 = str_replace("@#?", ' ', $subChain1);
+      echo strrev($subChain1)." ";
+
+      $keyNumber2 = strlen($message2)/2;
+      $subChain2 = substr($message2, 5, $keyNumber2);
+      $subChain2 = str_replace("@#?", ' ', $subChain2);
+      echo strrev($subChain2)." ";
+
+      $keyNumber3 = strlen($message3)/2;
+      $subChain3 = substr($message3, 5, $keyNumber3);
+      $subChain3 = str_replace("@#?", ' ', $subChain3);
+      echo strrev($subChain3);
+      
     ?> 
   </body>
 </html>
